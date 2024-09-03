2a654dcd3a4a8296cb2f17661054753259e3b949
raghu k <kadiyamraghu@gmail.com>
2024-09-03 04:14:01 +0000

    Initial commit

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def calculate_demographic_data(print_data=True):
+def calculate_demographic_data (print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
 
